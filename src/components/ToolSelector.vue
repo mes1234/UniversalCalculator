5c08b364498d6e5ccef5c71ae879ce33c4a81fe7
witek <witold.kepczynski@gmail.com>
2018-08-05 15:14:43 +0200

    update

---
+++ src/components/ToolSelector.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="ToolSelector">
+        <h2>Wybierz narzędzie</h2>
+        <ul>
+            <li v-for="product in saleProducts">
+                <span class="name">{{ product.name }}</span>
+                <span class="price">£{{ product.price }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        products(){
+            return this.$store.state.toolsList
+        },
+        saleProducts(){
+            return this.$store.getters.saleProducts
+        }
+    }
+}
+</script>
+
+<style>
+</style>
